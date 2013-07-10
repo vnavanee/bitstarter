3da369ecf9087a851d82d9707ad58b6de45752e3
V Neethan <example@stanford.edu>
2013-07-10 06:51:03 +0000

    Publishing index.html via web.js

--- web.js
+++ web.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
@@ -13,3 +14,4 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
