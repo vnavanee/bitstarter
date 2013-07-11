8549572774920b7c671cc4d87f7b8a197d2332e3
V Neethan <example@stanford.edu>
2013-07-11 03:25:27 +0000

    Fixed toString to toString()

--- web.js
+++ web.js
@@ -4,11 +4,12 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+var outfile = "index.html";
+var readfile =fs.readFileSync(outfile);
+var btos = readfile.toString();
 
-var fileread =fs.readFileSync('index.html');
-buf = new Buffer(fileread);
-response.send(buf.toString) ;
-});
+response.send(btos) ;
+fil});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
