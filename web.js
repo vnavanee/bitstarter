f87d4f6f8080d09922dfc479c4641bfa59c05892
V Neethan <example@stanford.edu>
2013-07-23 06:58:00 +0000

    modified to serve images

--- web.js
+++ web.js
@@ -1,17 +1,23 @@
 var express = require('express');
 var fs = require('fs');
 
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
+var app = express.createServer();
+app.use(express.logger());
+app.use(express.static(__dirname+'/public'));
+app.use(express.static(__dirname+'/images'));
+app.use('/', function(request, response) {
 var outfile = "index.html";
+
+//var img = "plazapagehome1.png";
+//var readimg = fs.readFileSync(img);
 var readfile =fs.readFileSync(outfile);
 var btos = readfile.toString();
 
 response.send(btos) ;
-fil});
+//response.send(readimg);
+});
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
